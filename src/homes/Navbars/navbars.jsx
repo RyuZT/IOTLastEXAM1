261cc9734dff75de27f9b5dd2762bba7afc6202e
RyuZT <vjhovantan123@gmail.com>
2024-05-07 13:56:23 +0700

    revisi

---
+++ src/homes/Navbars/navbars.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import"./navbars.css";
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-logo">
+       </div>  
+          <div class="navbar-nav">
+          <Link class="nav-link" to="./homes">Home</Link>
+            <Link class="nav-link active" aria-current="page" to="/">Pariwisata</Link>
+            <Link class="nav-link" to="./aboutus">About US</Link>
+            <Link class="nav-link" to="./game">Games</Link>
+            <Link class="nav-link" to="/contactus.jsx">Contact US</Link>
+          </div>
+        </div>
+    </div>
+  </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/homes/logos/logos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../Image/logo.png';
+import "./logo.css";
+
+function App() {
+  return (
+    <div>
+      <div className="logoContainer">
+        <img src={logo} alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
